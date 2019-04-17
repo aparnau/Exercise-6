5b49081af9b7f5a0bcbdd7f26229769160d23497
Aparna Unny <aparnaunny@gmail.com>
2019-04-17 16:48:57 +0530

    Add files via upload

---
+++ src/serverStatus.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+ <p> server status: {{ status }} </p>
+ <hr>
+ <button @click="changeStatus"> Change </button>
+</div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: 'Critical'
+    }
+  },
+  methods: {
+    changeStatus() {
+      this.status = 'Normal';
+    }
+  }
+}
+</script>
+
+<style>
+</style>
